d752e5c034b5267b6c65021d6b704a15705cbace
TUMO Labs Student <tumolabsstudent@TUMOs-iMac.local>
2023-06-17 17:12:50 +0400

    v.1.1

--- render.cpp
+++ render.cpp
@@ -1,51 +1,93 @@
 #include "functions.hpp"
 #include <iostream>
 
-void render_table() {
-  for (int row = 0; row < 9; ++row) {
-     int num = 8 - row;
-    if (row == 0) {
-	std::cout << "                     ";
-      for (int alf = 0; alf < 8; ++alf) {
-        char ch = 'a' + alf;
-        std::cout << "   " << ch << "  ";
+void render_table()
+{
+    std::string figures_char[8][8] = {
+        { "WR", "WN", "WB", "WQ", "WK", "WB", "WN", "WR" },
+        { "WP", "WP", "WP", "WP", "WP", "WP", "WP", "WP" },
+        { "  ", "  ", "  ", "  ", "  ", "  ", "  ", "  " },
+        { "  ", "  ", "  ", "  ", "  ", "  ", "  ", "  " },
+        { "  ", "  ", "  ", "  ", "  ", "  ", "  ", "  " },
+        { "  ", "  ", "  ", "  ", "  ", "  ", "  ", "  " },
+        { "BP", "BP", "BP", "BP", "BP", "BP", "BP", "BP" },
+        { "BR", "BN", "BB", "BQ", "BK", "BB", "BN", "BR" }
+    };
 
-        if (alf == 7) {
-          std::cout << std::endl;
+    std::string figures_emoji[8][8] = {
+        { " ♖ ", " ♘ ", " ♗ ", " ♕ ", " ♔ ", " ♗ ", " ♘ ", " ♖ " },
+        { " ♙ ", " ♙ ", " ♙ ", " ♙ ", " ♙ ", " ♙ ", " ♙ ", " ♙ " },
+        { "   ", "   ", "   ", "   ", "   ", "   ", "   ", "   " },
+        { "   ", "   ", "   ", "   ", "   ", "   ", "   ", "   " },
+        { "   ", "   ", "   ", "   ", "   ", "   ", "   ", "   " },
+        { "   ", "   ", "   ", "   ", "   ", "   ", "   ", "   " },
+        { " ♟ ", " ♟ ", " ♟ ", " ♟ ", " ♟ ", " ♟ ", " ♟ ", " ♟ " },
+        { " ♜ ", " ♞ ", " ♝ ", " ♛ ", " ♚ ", " ♝ ", " ♞ ", " ♜ " }
+    };
+
+    for (int row = 0; row < 9; ++row)
+    {
+        int num = 8 - row;
+        if (row == 0)
+        {
+            std::cout << "                     ";
+            for (int alf = 0; alf < 8; ++alf)
+            {
+                char ch = 'a' + alf;
+                std::cout << "   " << ch << "  ";
+
+                if (alf == 7)
+                {
+                    std::cout << std::endl;
+                }
+            }
         }
-      }
-    }
-	std::cout << "                     ";
-    for (int col1 = 0; col1 < 9; ++col1) {
-      if (col1 != 8) {
-        std::cout << "+ - - ";
-      } else {
-        std::cout << "+";
-      }
-    }
-    std::cout << std::endl;
-    if (row != 8) {
-	std::cout << "                   " << num << " " ;
-      for (int col2 = 0; col2 < 9; ++col2) {
-		if(col2 !=8){
-        std::cout << "|     ";
-}else{
-	std::cout << "| " << num;
-}
-      }
-    }
-    if (row == 8) {
-	std::cout << "                     ";
-      for (int alf = 0; alf < 8; ++alf) {
-        char ch = 'a' + alf;
-        std::cout << "   " << ch << "  ";
 
-        if (alf == 7) {
-          std::cout << std::endl;
+        std::cout << "                     ";
+        for (int col1 = 0; col1 < 9; ++col1)
+        {
+            if (col1 != 8)
+            {
+                std::cout << "+ - - ";
+            }
+            else
+            {
+                std::cout << "+";
+            }
         }
-      }
-    }
-    std::cout << std::endl;
-  }
 
+        std::cout << std::endl;
+        if (row != 8)
+        {
+            std::cout << "                   " << num << " ";
+            for (int col2 = 0; col2 < 9; ++col2)
+            {
+                if (col2 != 8)
+                {
+                    std::cout << "| " << figures_emoji[row][col2] << " ";
+                }
+                else
+                {
+                    std::cout << "| " << num;
+                }
+            }
+        }
+
+        if (row == 8)
+        {
+            std::cout << "                     ";
+            for (int alf = 0; alf < 8; ++alf)
+            {
+                char ch = 'a' + alf;
+                std::cout << "   " << ch << "  ";
+
+                if (alf == 7)
+                {
+                    std::cout << std::endl;
+                }
+            }
+        }
+
+        std::cout << std::endl;
+    }
 }
